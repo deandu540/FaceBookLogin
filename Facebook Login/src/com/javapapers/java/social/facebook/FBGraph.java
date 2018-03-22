9435a58b0c9089dadef3e74f1edc72034f4f61a7
deandu540 <yh.du@is-land.com.tw>
2018-03-22 16:23:58 +0800

    add project

---
+++ Facebook Login/src/com/javapapers/java/social/facebook/FBGraph.java
@@ -0,0 +1,65 @@
+package com.javapapers.java.social.facebook;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FBGraph {
+	private String accessToken;
+
+	public FBGraph(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getFBGraph() {
+		String graph = null;
+		try {
+
+//			String g = "https://graph.facebook.com/me?" + accessToken;
+			System.out.println("use token : " + accessToken);
+			
+			String g = "https://graph.facebook.com/v2.8/me?fields=id,name,first_name,email,gender&access_token=" + accessToken;
+//			g = URLEncoder.encode(g, "UTF-8");
+			System.out.println("URL : " + g);
+			URL u = new URL(g);
+			URLConnection c = u.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					c.getInputStream()));
+			String inputLine;
+			StringBuffer b = new StringBuffer();
+			while ((inputLine = in.readLine()) != null)
+				b.append(inputLine + "\n");
+			in.close();
+			graph = b.toString();
+			System.out.println(graph);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR in getting FB graph data. " + e);
+		}
+		return graph;
+	}
+
+	public Map<String, String> getGraphData(String fbGraph) {
+		Map<String, String> fbProfile = new HashMap<String, String>();
+		try {
+			JSONObject json = new JSONObject(fbGraph);
+			fbProfile.put("id", json.getString("id"));
+			fbProfile.put("first_name", json.getString("first_name"));
+			if (json.has("email"))
+				fbProfile.put("email", json.getString("email"));
+			if (json.has("gender"))
+				fbProfile.put("gender", json.getString("gender"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR in parsing FB graph data. " + e);
+		}
+		return fbProfile;
+	}
+}
